cc5d1de61d43f59a0d72afb2f6f142af7f9fce51
Gia Phuc <3123411228@sv.sgu.edu.vn>
2024-11-09 23:54:20 +0700

    Update cart page & checkout page header

---
+++ Js/CheckoutPage.js
@@ -0,0 +1,59 @@
+function goToHomePage() {
+    // Thay đổi URL để điều hướng về trang chủ
+    window.location.href = "index.html";
+}
+
+function goToLoginPage() {
+    window.location.href = "Login.html";
+}
+
+function goToCartPage() {
+    window.location.href = "CartPage.html";
+}
+
+function toggleMenu() {
+    const sidebarMenu = document.getElementById("sidebarMenu");
+  
+    // Kiểm tra vị trí hiện tại của sidebarMenu
+    if (sidebarMenu.style.left === "0px") {
+      // Nếu đang hiển thị, ẩn đi bằng cách di chuyển nó ra ngoài màn hình
+      sidebarMenu.style.left = "-300px";
+    } else {
+      // Nếu đang ẩn, hiển thị bằng cách di chuyển nó vào trong màn hình
+      sidebarMenu.style.left = "0px";
+    }
+}
+
+function openSearchAdvanced() {
+    document.getElementById("advanced-search").style.display = "block";
+    document.getElementById("advanced-search-overlay").style.display = "block";
+}
+  
+function closeSearchAdvanced() {
+    document.getElementById("advanced-search").style.display = "none";
+    document.getElementById("advanced-search-overlay").style.display = "none";
+}
+
+//Open Search Mobile
+function openSearchMb() {
+    document.querySelector(".header-middle-left").style.display = "none";
+    document.querySelector(".header-middle-center").style.display = "block";
+    document.querySelector(".header-middle-right-item.close").style.display =
+      "block";
+    let liItem = document.querySelectorAll(".header-middle-right-item.open");
+    for (let i = 0; i < liItem.length; i++) {
+      liItem[i].style.setProperty("display", "none", "important");
+    }
+}
+  
+  //Close Search Mobile
+function closeSearchMb() {
+    document.querySelector(".header-middle-left").style.display = "block";
+    document.querySelector(".header-middle-center").style.display = "none";
+    document.querySelector(".header-middle-right-item.close").style.display =
+      "none";
+    let liItem = document.querySelectorAll(".header-middle-right-item.open");
+    for (let i = 0; i < liItem.length; i++) {
+      liItem[i].style.setProperty("display", "block", "important");
+    }
+}
