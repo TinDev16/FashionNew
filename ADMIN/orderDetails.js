739db823000c35fe23048fcf61c07d428aad5fe2
Phanthien27 <3123411278@sv.sgu.edu.vn>
2024-11-25 19:38:20 +0700

    update orderDetail.html

--- ADMIN/orderDetails.js
+++ ADMIN/orderDetails.js
@@ -9,18 +9,21 @@ function showOrderDetails(orderId) {
             receiverPhone: "095463448",
             receiverAddress: "62, Bình Long, Quận Bình Tân, TP.HCM",
             notes: "Không",
-            product: "Áo Thun Nam Trơn Strength Contour Form Regular",
-            mv: 15364,
-            productQuantity: 2,
-            productPrice: 200000,
-            totalPrice: 400000,
+            product1: "Áo Thun Nam Trơn Strength Contour Form Regular",
+            productQuantity1: 2,
+            productPrice1: 200000,
+            totalPrice1: 400000,
+            product2: "Áo Sơ Mi Cuban Vải Xốp Nam Tay Ngắn Modern Vibe Form Relaxed",
+            productQuantity2: 1,
+            productPrice2: 200000,
+            totalPrice2: 200000,
             paymentMethod: "Thẻ tín dụng/Ghi nợ",
-            payment: 400000,
-            paymentAmount: 400000,
+            payment: 600000,
+            paymentAmount: 600000,
             promotion: 0,
             shippingFee: "Miễn phí",
-            total: 400000,
-            amountdue: 400000,
+            total: 600000,
+            amountdue: 600000,
         },
         "22050124171": {
             tg: "10/11/2024 - 14:30",
@@ -30,18 +33,21 @@ function showOrderDetails(orderId) {
             receiverPhone: "096789106",
             receiverAddress: "56,Hàm Nghi,Quận 1, TP.HCM",
             notes: "Nếu không có người nhận, xin để hàng tại cửa và gửi ảnh xác nhận.",
-            product: "Áo Polo Nam Họa Tiết Pattern Typography Form Regular",
-            mv:14953,
-            productQuantity: 3,
-            productPrice: 200000,
-            totalPrice: 600000,
+            product1: "Áo Polo Nam Họa Tiết Pattern Typography Form Regular",
+            productQuantity1: 2,
+            productPrice1: 200000,
+            totalPrice1: 400000,
+            product2: "Áo Sơ Mi Cuban Vải Xốp Nam Tay Ngắn Modern Vibe Form Relaxed",
+            productQuantity2: 3,
+            productPrice2: 200000,
+            totalPrice2: 600000,
             paymentMethod: "Tiền mặt",
-            payment: 600000,
-            paymentAmount: 600000,
+            payment: 1000000,
+            paymentAmount: 1000000,
             promotion: 0,
             shippingFee: "Miễn phí",
-            total: 600000,
-            amountdue: 600000,
+            total: 1000000,
+            amountdue: 1000000,
         },
         "28359124178": {
             tg: "15/11/2024 - 17:00",
@@ -51,18 +57,21 @@ function showOrderDetails(orderId) {
             receiverPhone: "0987073458",
             receiverAddress: "65,Hồng Bàng,Quận 5, TP.HCM",
             notes: "Gọi trước 30 phút trước khi giao để đảm bảo có người nhận.",
-            product: "Nón Nam Lưỡi Trai High-crown The Spiritual Kingdom Of Fashion",
-            mv:14359,
-            productQuantity: 1,
-            productPrice: 200000,
-            totalPrice: 200000,
+            product1: "Nón Nam Lưỡi Trai High-crown The Spiritual Kingdom Of Fashion",
+            productQuantity1: 2,
+            productPrice1: 200000,
+            totalPrice1: 400000,
+            product2: "Quần Short Nỉ Nam Đánh Bông Quai Túi Form Regular",
+            productQuantity2: 1,
+            productPrice2: 200000,
+            totalPrice2: 200000,
             paymentMethod: "VNPayQR",
-            payment: 200000,
-            paymentAmount: 200000,
+            payment: 600000,
+            paymentAmount: 600000,
             promotion: 0,
             shippingFee: "Miễn phí",
-            total: 200000,
-            amountdue: 200000,
+            total: 600000,
+            amountdue: 600000,
         },
     };
 
@@ -71,135 +80,92 @@ function showOrderDetails(orderId) {
     const order = orderDetails[orderId];
     if (order) {
         const orderDetailsHTML = `
-            <div class="row">
-        <div class="col-lg-8">
-            <div class="d-flex justify-content-between align-items-center bg-body-tertiary p-4 rounded">
-                <div>
-                    <p>
-                        <b class="text-dark">Đơn Hàng: </b>
-                        <span class="fs-6 text-primary" id="order-id">${orderId}</span>
-                    </p>
-                    <p class="text-dark">
-                        <span id="order-date">${order.tg}</span> |
-                        <span class="text-secondary">NV tư vấn:</span> Admin - admin@gmail.com
-                    </p>
+ <div class="order-info1">
+                <div class="order-details">
+                    <p><strong>Đơn Hàng:</strong> ${orderId}</p>
+                    <p>${order.tg}</p>
                 </div>
-     <div class="d-flex justify-content-end">
-                    <select class="form-select text-white fw-bold p-2 rounded-pill" id="status-select" style="width: 200px; background-color: #08397a; border: none; text-align: center;">
-                        <option value="chua_xu_ly">Chưa xử lý</option>
-                        <option value="da_xac_nhan">Đã xác nhận</option>
-                        <option value="giao_thanh_cong">Giao thành công</option>
-                        <option value="da_huy">Đã hủy</option>
-                    </select>
+                
+           
+               <div class="dropdown-container">
+  <select class="button-cancel1" id="status-select">
+    <option value="chua_xu_ly">Chưa xử lý</option>
+    <option value="da_xac_nhan">Đã xác nhận</option>
+    <option value="giao_thanh_cong">Giao thành công</option>
+    <option value="da_huy">Đã hủy</option>
+  </select>
+                </div>    
+                </div>      
+            
+            <div class="customer-info1">
+                <div class="box1">
+                    <p><strong>Khách Hàng</strong></p>
+                    <p>${order.customerName}</p>
+                    <p>${order.customerPhone}</p>
                 </div>
-
-            </div>
-
-            <div class="row" style="margin-top: 20px">
-                <div class="col-lg-6">
-                    <div class="card" style="width: 100%; height: 100%">
-                        <div class="card-header">Khách Hàng</div>
-                        <div class="card-body">
-                            <p class="text-black" id="customer-name">${order.customerName }</p>
-                            <p class="text-secondary" id="customer-phone">${order.customerPhone}</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-6">
-                    <div class="card" style="width: 100%">
-                        <div class="card-header">Người nhận</div>
-                        <div class="card-body">
-                            <p class="text-black" id="receiver-name">${order.receiverName}</p>
-                            <p class="text-secondary" id="receiver-phone">${order.receiverPhone}</p>
-                            <p class="text-secondary" id="receiver-address">${order.receiverAddress}</p>
-                        </div>
-                    </div>
+                <div class="box1">
+                    <p><strong>Người nhận</strong></p>
+                    <p>${order.receiverName}</p>
+                    <p>${order.receiverPhone}</p>
+                    <p>${order.receiverAddress}</p>
                 </div>
             </div>
-            <!-- New row for the notes section -->
-<div class="row" style="margin-top: 20px">
-    <div class="col-lg-12">
-        <div class="card" style="width: 100%">
-            <div class="card-header">Ghi Chú</div>
-            <div class="card-body">
-                <p class="text-secondary" id="order-notes">${order.notes}</p>
-            </div>
-        </div>
-    </div>
-</div>
-
-            <div style="margin-top: 20px">
-                <table class="table border" style="width: 100%">
+            <div class="product-info1">
+                <table>
                     <thead>
-                        <tr class="bg-secondary">
-                            <th scope="col">Tên Sản phẩm</th>
-                            <th scope="col">Số lượng</th>
-                            <th scope="col">Đơn Giá</th>
-                            <th scope="col">Tổng Tiền</th>
+                        <tr>
+                            <th>Tên Sản phẩm</th>
+                            <th>Số lượng</th>
+                            <th>Đơn Giá</th>
+                            <th>Tổng Tiền</th>
                         </tr>
                     </thead>
                     <tbody>
                         <tr>
-                            <th scope="row">
-                                ${order.product}
-                                <p class="text-secondary">Mã vạch:${order.mv}</p>
-                            </th>
-                            <td id="product-quantity">${order.productQuantity}</td>
-                            <td id="product-price">${order.productPrice.toLocaleString()}</td>
-                            <td id="product-total">${order.totalPrice.toLocaleString()}</td>
+                            <td>${order.product1}<br><small></small></td>
+                            <td>${order.productQuantity1}</td>
+                            <td>${order.productPrice1.toLocaleString()}</td>
+                            <td>${order.totalPrice1.toLocaleString()}</td>
                         </tr>
-                    </tbody>
+                   
+                        <tr>
+                            <td>${order.product2}<br><small></small></td>
+                            <td>${order.productQuantity2}</td>
+                            <td>${order.productPrice2.toLocaleString()}</td>
+                            <td>${order.totalPrice2.toLocaleString()}</td>
+                        </tr>
+                  
+                    </tbody> 
                 </table>
             </div>
-        </div>
-
-        <div class="col-lg-4">
-            <div>
-                <div class="card" style="width: 100%; height: 100%">
-                    <div class="card-header">Phương Thức Thanh Toán</div>
-                    <div class="card-body">
-                        <div class="d-flex justify-content-between">
-                            <p class="text-black">${order.paymentMethod}:</p>
-                            <p class="fw-bold fs-4 text-black" id="payment-cash">${order.payment.toLocaleString()}</p>
-                        </div>
-                      
-                    </div>
+            <div class="note1">
+                <p><strong>Ghi Chú</strong></p>
+                <p>${order.notes}</p>
+            </div>
+            
+            <div class="payment-info1">
+                <div class="box1">
+                    <p><strong>Phương Thức Thanh Toán</strong></p>
+                    <p>${order.paymentMethod}</p>
+                    <p class="amount1">${order.payment.toLocaleString()}</p>
+                </div>
+                <div class="box1">
+                    <p><strong>Tạm tính:</strong> ${order.paymentAmount.toLocaleString()}</p>
+                    <p>Khuyến mãi: ${order.promotion}</p>
+                    <p>Phí vận chuyển: ${order.shippingFee}</p>
+                    <p><strong>Thành tiền:</strong> ${order.total.toLocaleString()}</p>
+                    <p><strong>Cần thanh toán:</strong> <span class="amount1">${order.amountdue.toLocaleString()}</span></p>
                 </div>
             </div>
-
-            <div class="border p-2" style="margin-top: 20px">
-                <div class="d-flex justify-content-between">
-                    <p class="text-black">Tạm tính:</p>
-                    <p class="fw-bold fs-4 text-black" id="subtotal">${order.paymentAmount.toLocaleString()}</p>
-                </div>
-                <div class="d-flex justify-content-between">
-                    <p class="text-black">Khuyến mãi:</p>
-                    <p class="fw-bold fs-5 text-primary" id="promotion">${order.promotion.toLocaleString()}</p>
-                </div>
-                <div class="d-flex justify-content-between">
-                    <p class="text-black">Phí vận chuyển:</p>
-                    <p class="text-primary fw-bold fs-6" id="shipping-fee">${order.shippingFee}</p>
-                </div>
-                <div class="d-flex justify-content-between">
-                    <p class="text-black">Thành tiền:</p>
-                    <p class="fw-bold fs-5 text-black" id="total">${order.total.toLocaleString()}</p>
-                </div>
-                <hr />
-                <div class="d-flex justify-content-between">
-                    <p class="fw-bold fs-4 text-black">Cần thanh toán:</p>
-                    <p class="fw-bold fs-3 text-primary" id="amount-due">${order.amountdue.toLocaleString()}</p>
-                </div>
-            </div>
-            <button type="button" class="btn btn-primary p-4 fs-4" style="width: 100%; margin-top: 20px; background-color: #08397a; color: white; margin-bottom: 0px;" onclick="saveOrder();">
+            
+           
+            
+            <button 
+    type="button" 
+    class="btn btn-primary p-4 fs-4 button-close1" 
+    onclick="saveOrder();">
     Lưu
 </button>
-            <button type="button" class="btn btn-primary p-4 fs-4"  style="width: 100%; margin-top: 20px; background-color: #08397a; color: white;"  onclick="window.location.href='./orderManagement.html';">
-                Đóng
-            </button>
-        </div>
-    </div>
-</div>
-
 
 
         `;
